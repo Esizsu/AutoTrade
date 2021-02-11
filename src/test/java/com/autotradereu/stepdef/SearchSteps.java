a5f8d6f5b31014d8e6fa5a1d76be2c21ef3bd830
Esizsu <esra_izgi@hotmail.com>
2021-02-10 23:46:17 -0800

    SearchSteps.java updated

--- src/test/java/com/autotradereu/stepdef/SearchSteps.java
+++ src/test/java/com/autotradereu/stepdef/SearchSteps.java
@@ -16,18 +16,22 @@ public class SearchSteps {
     Logger logger = LoggerFactory.getLogger(SearchSteps.class);
     SearchPage searchPage = new SearchPage();
 
+
+
     @Given("User test with {string} browser")
-    /*bu methodu Cross Browser testing yapabilmek icin olusturduk.
-     2 sekilde Cross Browser testing yapabiliriz.
-     1-bu sekilde methodumuzu olusturup set.property ile,
-     2- yada Mydriver classimizdaki browserlar degerlerinden hangisini istiyorsak(chrome, firefox etc.
-      birini ConfigurationReader de  browserimiza gondeririz. o zman bu methoda ihtiyac yok.Bu saglikli bir yontem
-     */
 
     public void userTestWithBrowser(String brs) {
         if(brs.equals("chrome")){
            // ConfigurationReader.getProperty("browser"); ya bunu
             System.setProperty("browser","chrome");   // yada bunu yaptiririz
+
+            /*bu methodu Cross Browser testing yapabilmek icin olusturduk.
+     2 sekilde Cross Browser testing yapabiliriz.
+     1-bu sekilde methodumuzu olusturup set.property ile,
+     2- yada Mydriver classimizdaki browserlar degerlerinden hangisini istiyorsak(chrome, firefox etc.
+      birini ConfigurationReader de  browserimiza gondeririz. ozman bu methoda ihtiyac yok.
+      ConfigurationReader dan yapamak saglikli bir yontem
+     */
         }
     }
 
@@ -61,35 +65,5 @@ public class SearchSteps {
 
 
 
-    /*
-    orneke test methodunun Calisip calismadigini kontrol ettik.
-    @Test
-    public void test(){
-     MyDriver.get().get("https://www.autotrader.com/");
-        System.out.println("my "+ ConfigurationReader.getProperty("url")); //url'imi comut satirina yazdirir
-
-
-        // driverimizi cagiriyoruz web page'e land edebilmek icin.
-        burada MyDriver'in constructorini (Singilton) yani private yaptigimiz icin ve de metodlar static oldugu icin
-        Classin objectini olusturmadan direk metodlara ulasabiliyoruz.
-
-
-
-
-
-
-        Non-static( consturactor ile object olusturmamiz gerekiyor)
-        Normalde object olusturmak isini consturactor yaptigi icin, constructor'i public yapip methodlarimizi da public
-        yapinca, direkt o classin objectini olusturup (classname.obj = new classname) metodlara ulasiriz.
-        Hem class icinde hemde class disinda ulasiriz.(public cunku)
-
-        Static ( object olusturmadan, methodlara direk ulasamak icin)
-        Ama biz classimizin ulasilir olmasini yani object olusturulmasini istemiyorsak, o zaman Constriuctor'i private
-        (encapsulation) Singliton dizayn pattern'a cevirip methodlarimizi da static yapariz ki, object olusturmadan direkt,
-        methodlara ulasmak icin.
-
-    }
-
-     */
 }
 
